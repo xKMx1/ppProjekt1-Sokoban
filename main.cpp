c7d5a16ac818a887bff2a79dd7b5ece2e9dc2fe6
Kamil <kamilratajczyk00@gmail.com>
2022-12-02 23:05:47 +0100

    Changed game logic, added menu

--- main.cpp
+++ main.cpp
@@ -1,81 +1,30 @@
 #include <iostream>
-#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
+
+struct level
+{
+};
 
 const int mapX = 80;
 const int mapY = 25;
 
 void startScreen()
 { // funkcja wyswietlajaca ekran powitalny
-     cout << "                                /$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$  /$$$$$$$   /$$$$$$  /$$   /$$                                "
+     system("CLS");
+     cout << "\n"
+          << "SOKOBAN"
           << "\n";
-     cout << "                               /$$__  $$ /$$__  $$| $$  /$$/ /$$__  $$| $$__  $$ /$$__  $$| $$$ | $$                                "
+     cout << "Kamil Ratajczyk"
           << "\n";
-     cout << "                              | $$  \__/| $$  \ $$| $$ /$$/ | $$  \ $$| $$  \ $$| $$  \ $$| $$$$| $$                                "
+     cout << "s193345"
           << "\n";
-     cout << "                              |  $$$$$$ | $$  | $$| $$$$$/  | $$  | $$| $$$$$$$ | $$$$$$$$| $$ $$ $$                                "
-          << "\n";
-     cout << "                               \____  $$| $$  | $$| $$  $$  | $$  | $$| $$__  $$| $$__  $$| $$  $$$$                                "
-          << "\n";
-     cout << "                               /$$  \ $$| $$  | $$| $$\  $$ | $$  | $$| $$  \ $$| $$  | $$| $$\  $$$                                "
-          << "\n";
-     cout << "                              |  $$$$$$/|  $$$$$$/| $$ \  $$|  $$$$$$/| $$$$$$$/| $$  | $$| $$ \  $$                                "
-          << "\n";
-     cout << "                               \______/  \______/ |__/  \__/ \______/ |_______/ |__/  |__/|__/  \__/                                "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
-     cout << " /$$   /$$                         /$$ /$$       /$$$$$$$              /$$                                                 /$$      "
-          << "\n";
-     cout << "| $$  /$$/                        |__/| $$      | $$__  $$            | $$                                                | $$      "
-          << "\n";
-     cout << "| $$ /$$/   /$$$$$$  /$$$$$$/$$$$  /$$| $$      | $$  \ $$  /$$$$$$  /$$$$$$    /$$$$$$  /$$  /$$$$$$$ /$$$$$$$$ /$$   /$$| $$   /$$"
-          << "\n";
-     cout << "| $$$$$/   |____  $$| $$_  $$_  $$| $$| $$      | $$$$$$$/ |____  $$|_  $$_/   |____  $$|__/ /$$_____/|____ /$$/| $$  | $$| $$  /$$/"
-          << "\n";
-     cout << "| $$  $$    /$$$$$$$| $$ \ $$ \ $$| $$| $$      | $$__  $$  /$$$$$$$  | $$      /$$$$$$$ /$$| $$         /$$$$/ | $$  | $$| $$$$$$/ "
-          << "\n";
-     cout << "| $$\  $$  /$$__  $$| $$ | $$ | $$| $$| $$      | $$  \ $$ /$$__  $$  | $$ /$$ /$$__  $$| $$| $$        /$$__/  | $$  | $$| $$_  $$ "
-          << "\n";
-     cout << "| $$ \  $$|  $$$$$$$| $$ | $$ | $$| $$| $$      | $$  | $$|  $$$$$$$  |  $$$$/|  $$$$$$$| $$|  $$$$$$$ /$$$$$$$$|  $$$$$$$| $$ \  $$"
-          << "\n";
-     cout << "|__/  \__/ \_______/|__/ |__/ |__/|__/|__/      |__/  |__/ \_______/   \___/   \_______/| $$ \_______/|________/ \____  $$|__/  \__/"
-          << "\n";
-     cout << "                                                                                   /$$  | $$                     /$$  | $$          "
-          << "\n";
-     cout << "                                                                                  |  $$$$$$/                    |  $$$$$$/          "
-          << "\n";
-     cout << "                                                                                   \______/                      \______/           "
-          << "\n";
-     cout << "                                           /$$    /$$$$$$   /$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$                                   "
-          << "\n";
-     cout << "                                         /$$$$   /$$__  $$ /$$__  $$ /$$__  $$| $$  | $$| $$____/                                   "
-          << "\n";
-     cout << "                                /$$$$$$$|_  $$  | $$  \ $$|__/  \ $$|__/  \ $$| $$  | $$| $$                                        "
-          << "\n";
-     cout << "                               /$$_____/  | $$  |  $$$$$$$   /$$$$$/   /$$$$$/| $$$$$$$$| $$$$$$$                                   "
-          << "\n";
-     cout << "                              |  $$$$$$   | $$   \____  $$  |___  $$  |___  $$|_____  $$|_____  $$                                  "
-          << "\n";
-     cout << "                               \____  $$  | $$   /$$  \ $$ /$$  \ $$ /$$  \ $$      | $$ /$$  \ $$                                  "
-          << "\n";
-     cout << "                               /$$$$$$$/ /$$$$$$|  $$$$$$/|  $$$$$$/|  $$$$$$/      | $$|  $$$$$$/                                  "
-          << "\n";
-     cout << "                              |_______/ |______/ \______/  \______/  \______/       |__/ \______/                                   "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
-     cout << "                                                                                                                                    "
-          << "\n";
+
+     Sleep(1000);
+     system("CLS");
 }
 
 void fillMap(char tab[mapY][mapX]) // Funkcja tymczasowa, zapełniamy tablicę zerami i haszami
@@ -107,11 +56,43 @@ void genHero(int y1, int y2, int x1, int x2, char tab[mapY][mapX]) // Dostajemy 
 void movement()
 {
      char ruch = getchar();
-     printf("Przeczytano znak o numerze %i.", ruch);
+     switch (ruch) // Zależnie od wcisniętego klawisza wykonuje instrukcję
+     {             // WSAD (pruszanie się) - początek
+     case 119:     // w
+          cout << "w";
+          break;
+     case 115: // s
+          cout << "s";
+          break;
+     case 97: // a
+          cout << "a";
+          break;
+     case 100: // d
+          cout << "d";
+          break;
+     case 87: // W
+          cout << "W";
+          break;
+     case 83: // S
+          cout << "S";
+          break;
+     case 65: // A
+          cout << "A";
+          break;
+     case 68: // D
+          cout << "D";
+          break;
+          // WSAD - koniec
+     default:
+          cout << "Został wciśnięty nieznany klawisz";
+          break;
+     }
 }
 
 void genMap1() // wypisujemy na ekran mapę
 {
+     system("cls");
+
      char Map[mapY][mapX];
      fillMap(Map);
      genHero(14, 15, 45, 46, Map);
@@ -122,12 +103,43 @@ void genMap1() // wypisujemy na ekran mapę
           {
                cout << Map[i][j];
           }
+          cout << "\n";
+     }
+}
+
+int genMenu()
+{
+     system("cls");
+     cout << "Wybierz:"
+          << "\n";
+     cout << "(1) Rozpocznij gre od pierwszego etapu"
+          << "\n";
+     cout << "(2) Opusc gre"
+          << "\n";
+     while (1)
+     {
+          char znak = getchar();
+
+          if (znak == 49)
+          {
+               return 1;
+          }
+          else if (znak == 50)
+          {
+               exit(0);
+          }
+          else
+          {
+               cout << "Wybrales niepoprawny znak! Mozesz wybrac 1 lub 2."
+                    << "\n";
+          }
      }
 }
 
 int main()
 {
-     // startScreen();
-     genMap1();
-     movement();
+     startScreen();
+     genMenu();
+
+     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+     fillMap(Map);
